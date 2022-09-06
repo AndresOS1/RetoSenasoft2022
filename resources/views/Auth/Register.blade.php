77c93b7abfeb36e849c5d8d15fcf8b7ebb5473e4
Juanyonda13 <juan.yonda@misena.edu.com>
2022-09-06 13:36:42 -0500

    register medio hecho

---
+++ resources/views/Auth/Register.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+     <div class="col-12 d-flex justify-content-center" style="height: 100vh;">
+         <div class="col-8">
+            <section class="w-100 border shadow-lg d-flex h-50">
+                <article class="w-50 h-100">
+                    
+                </article>
+                <article class="w-50 h-100 ">
+                     <form action="" class="w-100 h-100">
+                         <div class="w-100">
+                            <input type="text" class="for-control w-100">
+                         </div>
+                         <div class="w-100">
+                            <input type="text" class="for-control w-100">
+                         </div>
+                     </form>
+                </article>
+            </section>
+         </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
